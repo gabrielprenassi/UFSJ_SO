d6f7cd9cd8a5c77de392efc84e94e324624ccd77
gabrielprenassi <prenassigabriel@gmail.com>
2024-11-18 15:07:49 -0300

    updates

--- trabalho1/threads_race.c
+++ trabalho1/threads_race.c
@@ -8,43 +8,51 @@
 #define TARGET 99
 
 // Variaveis
-int vencedor = -1;
+int winner = -1;
 pthread_mutex_t mutex;
-pthread_cond_t cond;
+pthread_cond_t start_cond;
+int threads_ready = 0;
 
 // Funcao que define o comportamento de cada thread
 void* thread_func(void* arg) {
     int id = *(int*)arg;
     free(arg);
 
-    int contador_local = 0;
+    int local_counter = 0;
 
-    while (contador_local <= TARGET) {
+    // Cada thread fica esperando ate que todas as outras estejam prontas para comecar
+    pthread_mutex_lock(&mutex);
+    threads_ready++;
+    while (threads_ready != NUM_THREADS){
+    	pthread_cond_wait(&start_cond, &mutex);
+    }
+    pthread_cond_broadcast(&start_cond);
+    pthread_mutex_unlock(&mutex);
+
+    while (local_counter <= TARGET) {
         pthread_mutex_lock(&mutex);
 
         // Verifica se alguma thread ja ganhou
-        if (vencedor != -1) {
+        if (winner != -1) {
             pthread_mutex_unlock(&mutex);
             pthread_exit(NULL);
         }
 
-        // Simula uma chance (20%) de perda de contagem
-        if (rand() % 5 == 0) {
-            contador_local--;
-            if (contador_local < 0) {
-                contador_local = 0;
+        if (rand() % 5 == 0) { // Simula uma chance (20%) de perda de contagem
+            local_counter--;
+            if (local_counter < 0) {
+                local_counter = 0;
             }
-            printf("Thread %d: perdeu um ponto, contador local = %d\n", id, contador_local);
+            printf("Thread %d: perdeu um ponto, contador local = %d\n", id, local_counter);
         } else {
-            contador_local++;
-            printf("Thread %d: contador local = %d\n", id, contador_local);
+            local_counter++;
+            printf("Thread %d: contador local = %d\n", id, local_counter);
         }
 
         // Verifica se a thread atingiu o alvo
-        if (contador_local == TARGET) {
-            vencedor = id;
+        if (local_counter == TARGET) {
+            winner = id;
             printf("Thread %d ganhou a corrida!\n", id);
-            pthread_cond_broadcast(&cond);
         }
         
         pthread_mutex_unlock(&mutex);
@@ -56,11 +64,11 @@ void* thread_func(void* arg) {
     return NULL;
 }
 
-// Funcao principal
+// Função principal
 int main() {
     pthread_t threads[NUM_THREADS];
     pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&cond, NULL);
+    pthread_cond_init(&start_cond, NULL);
 
     // Inicializa a semente do gerador de numeros aleatorios
     srand(time(NULL));
@@ -72,20 +80,13 @@ int main() {
         pthread_create(&threads[i], NULL, thread_func, id);
     }
 
-    // Thread principal aguarda o termino da corrida
-    pthread_mutex_lock(&mutex);
-    while (vencedor == -1) {
-        pthread_cond_wait(&cond, &mutex); // Thread principal acorda quando a variavel vencedor for alterada
-    }
-    pthread_mutex_unlock(&mutex);
-
-    // Thread principal espera as threads terminarem
+    // Thread princiapl espera as threads terminarem
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
 
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
+    pthread_cond_destroy(&start_cond);
 
     return  0;
 }
